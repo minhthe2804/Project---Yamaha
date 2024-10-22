d58eba98653b5db2d5a28daeb6f38bfe27de7319
Minh The <minhthe2804@gmail.com>
2024-10-23 01:48:38 +0700

    Create UI Policy to Pages/Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from '~/components/Banner'
 import Products from '~/components/Products'
+import Policy from './components/Policy'
 
 export default function Home() {
     return (
         <div>
             <Banner />
             <Products />
+            <Policy />
         </div>
     )
 }
---
+++ src/pages/Home/components/Policy/Policy.tsx
@@ -0,0 +1,49 @@
+export default function Policy() {
+    return (
+        <div className='w-full bg-[#c7c7c7] mt-[80px] py-[102px]'>
+            <div className='max-w-[1198px] mx-auto'>
+                <div className='grid grid-cols-12 gap-8'>
+                    <div className='col-span-4'>
+                        <div className='border-[1px] border-[#c0c0c0] rounded-[12px] relative pt-[45px] pl-[25px] pb-[35px]'>
+                            <p className='text-[15px] font-[550] text-[#181b23] opacity-[0.9]'>TƯ VẤN</p>
+                            <p className='text-[14.5px] leading-[22px] w-[305px] mt-[14px] text-[#44464c]'>
+                                Tư vấn nhanh chóng, chính xác với đầy đủ các dòng xe hiện đại.
+                            </p>
+                            <img
+                                src='https://theme.hstatic.net/200000281285/1000677821/14/policy_icon1.png?v=848'
+                                alt=''
+                                className='absolute right-[-21px] top-[50%] tranfrom -translate-y-1/2'
+                            />
+                        </div>
+                    </div>
+                    <div className='col-span-4'>
+                        <div className='border-[1px] border-[#c0c0c0] rounded-[12px] relative pt-[45px] pl-[25px] pb-[35px] '>
+                            <p className='text-[15px] font-[550] text-[#181b23] opacity-[0.9]'>THANH TOÁN</p>
+                            <p className='text-[14.5px] leading-[22px] w-[305px] mt-[14px] text-[#44464c]'>
+                                Chấp nhận thanh toán bằng thẻ, tiền mặt hoặc các hình thức khác
+                            </p>
+                            <img
+                                src='https://theme.hstatic.net/200000281285/1000677821/14/policy_icon2.png?v=848'
+                                alt=''
+                                className='absolute right-[-21px] top-[50%] tranfrom -translate-y-1/2'
+                            />
+                        </div>
+                    </div>
+                    <div className='col-span-4'>
+                        <div className='border-[1px] border-[#c0c0c0] rounded-[12px] relative pt-[45px] pl-[25px] pb-[35px] '>
+                            <p className='text-[15px] font-[550] text-[#181b23] opacity-[0.9]'>HỖ TRỢ</p>
+                            <p className='text-[14.5px] leading-[22px] w-[320px] mt-[14px] text-[#44464c]'>
+                                Luôn luôn hỗ trợ online khách hàng 24/7 với tổng đài của chúng tôi
+                            </p>
+                            <img
+                                src='https://theme.hstatic.net/200000281285/1000677821/14/policy_icon3.png?v=848'
+                                alt=''
+                                className='absolute right-[-21px] top-[50%] tranfrom -translate-y-1/2'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
