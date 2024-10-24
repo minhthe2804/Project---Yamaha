0ab1e802e6a1363297e0bc2d556117eeb97314db
Minh The <minhthe2804@gmail.com>
2024-10-24 15:25:35 +0700

    Implement logic to call the Product api and filter products by categories

--- src/components/Products/components/Categories/Categories.tsx
+++ src/components/Products/components/Categories/Categories.tsx
@@ -1,24 +1,54 @@
+import { categories } from '~/constants/Categories'
+import { createSearchParams, useNavigate } from 'react-router-dom'
+import { path } from '~/constants/path'
+import useQueryConfig from '~/hooks/useQueryConfig'
+import classNames from 'classnames'
+
 export default function Categories() {
+    const queryConfig = useQueryConfig()
+    const activeCategory = queryConfig.category
+    const navigate = useNavigate()
+
+
+    const handleCategory = (category: string) => {
+        navigate({
+            pathname: path.home,
+            search: createSearchParams({
+                ...queryConfig,
+                category
+            }).toString()
+        })
+    }
+
     return (
         <div className='flex justify-center items-center mt-[47px] gap-10'>
-            <div className='flex justify-center items-center gap-2 cursor-pointer group'>
-                <p className='text-lg font-[500] group-hover:text-[#ff3237] transiton duration-300 ease-in'>
-                    SẢN PHẨM NỔI BẬT
-                </p>
-                <div className='w-[8px] h-[8px] bg-black rotate-45 group-hover:bg-[#ff3237] transiton duration-300 ease-in'></div>
-            </div>
-            <div className='flex justify-center items-center gap-2 cursor-pointer group'>
-                <p className='text-lg font-[500] group-hover:text-[#ff3237] transiton duration-300 ease-in'>
-                    SẢN PHẨM MỚI
-                </p>
-                <div className='w-[8px] h-[8px] bg-black rotate-45 group-hover:bg-[#ff3237] transiton duration-300 ease-in'></div>
-            </div>
-            <div className='flex justify-center items-center gap-2 cursor-pointer group'>
-                <p className='text-lg font-[500] group-hover:text-[#ff3237] transiton duration-300 ease-in'>
-                    SẢN PHẨM BÁN CHẠY
-                </p>
-                <div className='w-[8px] h-[8px] bg-black rotate-45 group-hover:bg-[#ff3237] transiton duration-300 ease-in'></div>
-            </div>
+            {categories.map((category, index) => (
+                <div
+                    className='flex justify-center items-center gap-2 cursor-pointer group'
+                    key={index}
+                    onClick={() => handleCategory(category)}
+                >
+                    <p
+                        className={classNames(
+                            'text-lg font-[500] group-hover:text-[#ff3237] transiton duration-300 ease-in',
+                            {
+                                'text-[#ff3237]': category === activeCategory
+                            }
+                        )}
+                    >
+                        {category}
+                    </p>
+                    <div
+                        className={classNames(
+                            'w-[8px] h-[8px] rotate-45 group-hover:bg-[#ff3237] transiton duration-300 ease-in',
+                            {
+                                'bg-[#ff3237]': category === activeCategory,
+                                'bg-black': category !== activeCategory
+                            }
+                        )}
+                    ></div>
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/hooks/useQueryConfig.ts
@@ -0,0 +1,25 @@
+import isUndefined from 'lodash/isUndefined'
+import omitBy from 'lodash/omitBy'
+
+import useQueryParams from './useQueryParams'
+import { ProductsConfig } from '~/types/Products.type'
+
+export type QueryConfig = {
+    [key in keyof ProductsConfig]: string
+}
+
+export default function useQueryConfig() {
+    const queryParams: QueryConfig = useQueryParams()
+    const queryConfig: QueryConfig = omitBy(
+        {
+            page: queryParams.page,
+            limit: queryParams.limit,
+            price_max: queryParams.price_max,
+            price_min: queryParams.price_min,
+            category: queryParams.category || 'SẢN PHẨM MỚI NHẤT',
+            vendor: queryParams.vendor
+        },
+        isUndefined
+    )
+    return queryConfig
+}
