49eaca35834eb2569c3f5cbc69866e5f150be582
Minh The <minhthe2804@gmail.com>
2024-10-25 22:38:42 +0700

    Optimal component BreadCrumb

--- src/components/BreadCrumb/BreadCrumb.tsx
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -1,12 +1,15 @@
-import { breadCrumb } from "~/constants/BreadCrumb";
+interface Props {
+    heading?: string
+    title?: string
+}
 
-export default function BreadCrumb() {
+export default function BreadCrumb({ heading, title }: Props) {
     return (
         <div className='w-full h-[300px] bg-[url("https://theme.hstatic.net/200000281285/1000677821/14/breadcrumb_bg5.png?v=848")] bg-no-repeat bg-center bg-cover py-[30px] relative'>
             <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#00000080]' />
             <div className='w-full h-full flex flex-col items-center justify-center text-white relative z-[2] gap-1'>
-                <h2 className='text-[22px] font-semibold'>{breadCrumb.register.heading}</h2>
-                <p className='text-[15px] font-[500]'>{breadCrumb.register.title}</p>
+                <h2 className='text-[22px] font-semibold'>{heading}</h2>
+                <p className='text-[15px] font-[500]'>{title}</p>
             </div>
         </div>
     )
