9335103805cf176b48511774e26c6855e0dbeada
Minh The <minhthe2804@gmail.com>
2024-11-26 15:26:54 +0700

    fix the phone conditions in userSchema at rules

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -21,6 +21,7 @@ export const userSchema = yup.object({
     username: yup.string().required('Bạn cần phải nhập họ tên').max(160, 'Độ dài tối đa là 160 kí tự'),
     phone: yup
         .string()
+        .required("Bạn cần phải nhập số điện thoại")
         .matches(/^(0|\+84)(3[2-9]|5[2689]|7[06-9]|8[1-5]|9[0-9])[0-9]{7}$/, 'Số điện thoại không đúng định dạng')
         .max(20, 'Độ dài tối đa là 20 kí tự'),
     address: yup.string().required('Bạn cần phải nhập địa chỉ').max(160, 'Độ dài tối đa là 160 kí tự')
