22ed6e7bbb50be2a90b9daeba426f913ad0c3221
Minh The <minhthe2804@gmail.com>
2025-01-08 00:35:52 +0700

    fix lại type của product

--- src/types/Products.type.ts
+++ src/types/Products.type.ts
@@ -1,8 +1,27 @@
 export interface Product {
+    available: boolean
+    compare_at_price_max: number
+    compare_at_price_min: number
+    compare_at_price_varies: boolean
+    compare_at_price: number
+    content: null
     description: string
     featured_image: string
-    id: number
+    handle: string
+    id: string
     images: string[]
+    media: {
+        alt: null
+        id: number
+        position: number
+        preview_image: {
+            src: string
+        }
+        external_id: null
+        host: null
+        media_type: string
+        src: string
+    }[]
     options: {
         name: string
         position: number
@@ -11,12 +30,46 @@ export interface Product {
     price: number
     price_max: number
     price_min: number
+    price_varies: boolean
+    tags: never[]
+    template_suffix: null
     title: string
     category: string
     type: string
+    url: string
+    pagetitle: string
+    metadescription: string
+    variants: {
+        id: number
+        barcode: null
+        available: boolean
+        price: number
+        sku: null
+        option1: string
+        option2: string
+        option3: string
+        options: string[]
+        inventory_quantity: number
+        old_inventory_quantity: number
+        title: string
+        weight: number
+        compare_at_price: number
+        inventory_management: string
+        inventory_policy: string
+        selected: boolean
+        url: null
+        featured_image: {
+            id: number
+            position: number
+            src: string
+            alt: null
+            variant_ids: number[]
+        }
+    }
     vendor: string
     published_at: string
     created_at: string
+    not_allow_promotion: boolean
     quantity: number
 }
 
