7193390d8522a50d7061420a7bb021f765ce2a7f
Minh The <minhthe2804@gmail.com>
2025-01-20 13:29:51 +0700

    Update Images Banner

--- src/constants/Banner.ts
+++ src/constants/Banner.ts
@@ -1,15 +1,10 @@
-export const banner = [
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img1.jpg?v=848',
+import image1 from '/src/assets/images/banner/ms_banner_img1.webp'
+import image2 from '/src/assets/images/banner/ms_banner_img2.webp'
+import image3 from '/src/assets/images/banner/ms_banner_img3.jpg'
+import image4 from '/src/assets/images/banner/ms_banner_img4.webp'
+import image5 from '/src/assets/images/banner/ms_banner_img5.jpg'
+import image6 from '/src/assets/images/banner/ms_banner_img6.jpg'
+import image7 from '/src/assets/images/banner/ms_banner_img7.webp'
+import image8 from '/src/assets/images/banner/ms_banner_img8.webp'
 
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img3.jpg?v=848',
-
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img4.jpg?v=848',
-
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img5.jpg?v=848',
-
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img6.jpg?v=848',
-
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img7.jpg?v=848',
-
-    'https://theme.hstatic.net/200000281285/1000677821/14/ms_banner_img8.jpg?v=848'
-]
+export const banner = [image1, image2, image3, image4, image5, image6, image7, image8]
